ce6c9a2b1d5f5345d74ef3eb50a87add5853cf7b
Angeloyo <33759346+Angeloyo@users.noreply.github.com>
2024-05-16 09:22:11 +0200

    first upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,css}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'custom': ['Teachers', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
