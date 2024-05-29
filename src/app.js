2e7c5845d94cdb8b90a100e94c82afb31ab766d6
Angeloyo <33759346+Angeloyo@users.noreply.github.com>
2024-05-29 20:43:01 +0200

    working

--- src/app.js
+++ src/app.js
@@ -155,28 +155,32 @@ function loadGallery(showLoader = true) {
             const imageUrl = `https://${bucketName}.s3.${AWS.config.region}.amazonaws.com/${item.Key}`;
         
             const container = document.createElement('div');
-            container.className = 'gallery-item-container relative';
+            container.className = 'gallery-item-container inline-block relative ';
         
             const a = document.createElement('a');
             a.href = imageUrl;
 
             const img = document.createElement('img');
             img.src = imageUrl;
-            img.className = 'gallery-item ';
+            img.className = 'block w-full h-auto';
         
             a.appendChild(img);
             container.appendChild(a);
         
             const deleteIcon = document.createElement('div');
-            deleteIcon.className = 'delete-icon absolute top-2 right-2 w-6 h-6 bg-red-600 text-white text-center rounded-full cursor-pointer flex items-center justify-center';
-            deleteIcon.innerHTML = 'X';
+            deleteIcon.className = 'hidden delete-icon absolute top-2 right-2 p-1 bg-red-400 hover:bg-red-500 text-white text-center rounded-full cursor-pointer flex items-center justify-center';
+            deleteIcon.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-3">
+                <path fill-rule="evenodd" d="M5 3.25V4H2.75a.75.75 0 0 0 0 1.5h.3l.815 8.15A1.5 1.5 0 0 0 5.357 15h5.285a1.5 1.5 0 0 0 1.493-1.35l.815-8.15h.3a.75.75 0 0 0 0-1.5H11v-.75A2.25 2.25 0 0 0 8.75 1h-1.5A2.25 2.25 0 0 0 5 3.25Zm2.25-.75a.75.75 0 0 0-.75.75V4h3v-.75a.75.75 0 0 0-.75-.75h-1.5ZM6.05 6a.75.75 0 0 1 .787.713l.275 5.5a.75.75 0 0 1-1.498.075l-.275-5.5A.75.75 0 0 1 6.05 6Zm3.9 0a.75.75 0 0 1 .712.787l-.275 5.5a.75.75 0 0 1-1.498-.075l.275-5.5a.75.75 0 0 1 .786-.711Z" clip-rule="evenodd" />
+            </svg>`
+          ;
             deleteIcon.onclick = function() {
                 deleteImage(item.Key);
             };        
             container.appendChild(deleteIcon);
 
             const tagContainer = document.createElement('div');
-            tagContainer.className = 'tag-container absolute bottom-0 left-0 w-full text-white bg-black bg-opacity-50 hidden';
+            tagContainer.className = 'p-2 text-center absolute bottom-0 left-0 w-full text-white bg-black bg-opacity-50 hidden';
             container.appendChild(tagContainer);
 
             fetchTags(item.Key, function(tags) {
